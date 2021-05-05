0fa3f6e9917007be774a741ea418a97794458710
Tulio <tuliotrindade99@gmail.com>
2021-05-05 16:47:28 -0300

    desafio 7 concluido

--- script.js
+++ script.js
@@ -14,5 +14,38 @@ for(let i=0;i<n;i+=1){
     colunas.className="pixel"
 }
 }
+
+let color1=document.querySelectorAll(".color")[0]
+let color2=document.querySelectorAll(".color")[1]
+let color3=document.querySelectorAll(".color")[2]
+let color4=document.querySelectorAll(".color")[3]
+
+color1.addEventListener("click", function(){
+    color1.className="color selected"
+    color2.className="color"
+    color3.className="color"
+    color4.className="color"
+})
+color2.addEventListener("click", function(){
+    color1.className="color"
+    color2.className="color selected"
+    color3.className="color"
+    color4.className="color"
+})
+color3.addEventListener("click", function(){
+    color1.className="color"
+    color2.className="color"
+    color3.className="color selected"
+    color4.className="color"
+})
+color4.addEventListener("click", function(){
+    color1.className="color"
+    color2.className="color"
+    color3.className="color"
+    color4.className="color selected"
+})
+
+
+
 window.onload=document.querySelectorAll(".color")[0].className="color selected";
 
