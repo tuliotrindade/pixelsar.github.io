ebd04ad0998b788058470c5965233d0971b7a5da
Tulio <tuliotrindade99@gmail.com>
2021-05-05 15:47:22 -0300

    desafio 4 concluido

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+const tabela=document.querySelector("#pixel-board")
 document.querySelectorAll(".color")[0].style.background="black"
 document.querySelectorAll(".color")[1].style.background="red"
 document.querySelectorAll(".color")[2].style.background="green"
 document.querySelectorAll(".color")[3].style.background="blue"
+let n=5;
+
+for(let i=0;i<n;i+=1){
+    let linhas=document.createElement("tr");
+    tabela.appendChild(linhas);
+    linhas.className
+    for(let i=0;i<n;i+=1){
+    let colunas=document.createElement("td");
+    tabela.appendChild(colunas);
+    colunas.className="pixel"
+}
+}
+
