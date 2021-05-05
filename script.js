b6fbbcbc1c2e93fd78982b85a5f2be8113a429e5
Tulio <tuliotrindade99@gmail.com>
2021-05-05 15:04:24 -0300

    requisito 2 concluido

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelectorAll(".color")[0].style.background="black"
+document.querySelectorAll(".color")[1].style.background="red"
+document.querySelectorAll(".color")[2].style.background="green"
+document.querySelectorAll(".color")[3].style.background="blue"
